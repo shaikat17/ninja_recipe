f7d77ddba0d67b06068fde4a1469e280e65d28e9
shaikat17 <shaikatpal56@gmail.com>
2023-06-21 01:11:26 +0600

    usefetch hook added and its working

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,18 @@
-
+import { useFetch } from "../../hooks/useFetch";
 
 const Home = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  const { data, isPending, error } = useFetch("./db.json");
+  console.log(isPending);
+
+  if (isPending) return <h1>Loading...</h1>;
+
+  return (
+    <div className="home">
+      {error && <p className="error">{error}</p>}
+      {isPending && <p className="loading">Loading...</p>}
+      {data && data.recipes.map((recipe) => <h2 key={recipe.id}>{recipe.title}</h2>)}
+    </div>
+  );
 };
 
 export default Home;
