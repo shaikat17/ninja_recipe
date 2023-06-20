815db3c535c4b9592286b88ade39d4d6b7208e1e
shaikat17 <shaikatpal56@gmail.com>
2023-06-21 00:41:21 +0600

    created the all routes for the projects

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,9 +1,31 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "../pages/home/Home";
+import SharedLayout from "../layout/SharedLayout";
+import Create from "../pages/create/Create";
+import Search from "../pages/search/Search";
+import Recipe from "../pages/recipe/Recipe";
 
 export const router = createBrowserRouter([
     {
       path: "/",
-      element: <Home />,
+      element: <SharedLayout />,
+      children: [
+        {
+            path: "/",
+            element: <Home />
+        },
+        {
+            path: "/create",
+            element: <Create />
+        },
+        {
+            path: "/search",
+            element: <Search />
+        },
+        {
+            path: "/recipes/:id",
+            element: <Recipe />
+        },
+      ]
     },
   ]);
